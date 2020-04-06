a2bbf09385d499477d51eb264ca0b015ce4e9366
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2020-04-05 20:21:29 -0400

    Use the Linux GPIO Character Device API

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,63 @@
-extern crate sysfs_gpio;
+use gpio_cdev::{Chip, EventRequestFlags, LineRequestFlags};
 
 use std::thread::sleep;
 use std::time::Duration;
-use sysfs_gpio::{Direction, Pin};
 
 fn main() {
-    let red_led = Pin::new(25);
-    let yellow_led = Pin::new(8);
-    let green_led = Pin::new(7);
-    let buzzer = Pin::new(15);
-    let button = Pin::new(1);
-
-    red_led.export().unwrap();
-    red_led.set_direction(Direction::Out).unwrap();
-
-    yellow_led.export().unwrap();
-    yellow_led.set_direction(Direction::Out).unwrap();
-
-    green_led.export().unwrap();
-    green_led.set_direction(Direction::Out).unwrap();
-
-    buzzer.export().unwrap();
-    buzzer.set_direction(Direction::Out).unwrap();
-
-    button
-        .with_exported(|| {
-            button.set_direction(Direction::In)?;
-
-            // The red led is on by default
-            red_led.set_value(1).unwrap();
-
-            // The button's initial state is High, or 1.
-            let mut prev_val: u8 = 1;
-            loop {
-                let val = button.get_value()?;
-                if val != prev_val {
-                    if val == 1 {
-                        sleep(Duration::from_millis(1000));
-                        // Red lef off
-                        red_led.set_value(0).unwrap();
-
-                        // Green led on and beep it the buzzer 4 times
-                        green_led.set_value(1).unwrap();
-                        for _x in 0..3 {
-                            buzzer.set_value(1).unwrap();
-                            sleep(Duration::from_millis(500));
-                            buzzer.set_value(0).unwrap();
-                            sleep(Duration::from_millis(500));
-                        }
-
-                        // Yellow led on for 2 seconds and turning off green led
-                        yellow_led.set_value(1).unwrap();
-                        green_led.set_value(0).unwrap();
-                        sleep(Duration::from_millis(2000));
-
-                        // Red light on again.
-                        red_led.set_value(1).unwrap();
-                        yellow_led.set_value(0).unwrap();
-                    }
-                    prev_val = val;
-                }
-                sleep(Duration::from_millis(10));
-            }
-        })
+    let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+    let red_led = chip.get_line(25).unwrap();
+    let red_led_handle = red_led
+        .request(LineRequestFlags::OUTPUT, 0, "red_led")
         .unwrap();
+
+    let yellow_led = chip.get_line(8).unwrap();
+    let yellow_led_handle = yellow_led
+        .request(LineRequestFlags::OUTPUT, 0, "yellow_led")
+        .unwrap();
+
+    let green_led = chip.get_line(7).unwrap();
+    let green_led_handle = green_led
+        .request(LineRequestFlags::OUTPUT, 0, "red_led")
+        .unwrap();
+
+    let buzzer = chip.get_line(15).unwrap();
+    let buzzer_handle = buzzer
+        .request(LineRequestFlags::OUTPUT, 0, "buzzer")
+        .unwrap();
+
+    let button = chip.get_line(1).unwrap();
+
+    // Red light on by default.
+    red_led_handle.set_value(1).unwrap();
+
+    for _event in button
+        .events(
+            LineRequestFlags::INPUT,
+            EventRequestFlags::RISING_EDGE,
+            "button-input",
+        )
+        .unwrap()
+    {
+        sleep(Duration::from_millis(1000));
+        // Red lef off
+        red_led_handle.set_value(0).unwrap();
+
+        // Green led on and beep it the buzzer 4 times
+        green_led_handle.set_value(1).unwrap();
+        for _x in 0..3 {
+            buzzer_handle.set_value(1).unwrap();
+            sleep(Duration::from_millis(500));
+            buzzer_handle.set_value(0).unwrap();
+            sleep(Duration::from_millis(500));
+        }
+
+        // Yellow led on for 2 seconds and turning off green led
+        yellow_led_handle.set_value(1).unwrap();
+        green_led_handle.set_value(0).unwrap();
+        sleep(Duration::from_millis(2000));
+
+        // Red light on again.
+        red_led_handle.set_value(1).unwrap();
+        yellow_led_handle.set_value(0).unwrap();
+    }
 }
